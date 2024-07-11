f6cfc746326b648b093af6ca9aff4f907237dc9d
nadi726 <16650084+nadi726@users.noreply.github.com>
2024-07-11 11:30:09 +0300

    Add operation variables

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+// operator functions
 const addOperator = (a, b) => a + b;
 const subtractOperator = (a, b) => a - b;
 const multiplyOperator = (a, b) => a * b;
 const divideOperator = (a, b) => a / b;
+
+// global operation variables
+let leftOperand;
+let operator;
+let rightOperand;
